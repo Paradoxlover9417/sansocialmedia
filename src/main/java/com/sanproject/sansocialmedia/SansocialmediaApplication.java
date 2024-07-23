094455a9f097a73fd8cdbf16012cba567ee28aa7
Paradoxlover9417 <tksgk853@gmail.com>
2024-07-23 15:39:26 +0900

    first_commit

---
+++ src/main/java/com/sanproject/sansocialmedia/SansocialmediaApplication.java
@@ -0,0 +1,14 @@
+package com.sanproject.sansocialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class SansocialmediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SansocialmediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanproject/sansocialmedia/SansocialmediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanproject.sansocialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SansocialmediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
